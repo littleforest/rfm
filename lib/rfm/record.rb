f4d82afccdc2ec45e615c9e43fe4cbb7c8ab027a
wbr <wbr@mac.com>
2011-12-01 04:31:09 -0800

    Fixes to Record getters & setters.

--- lib/rfm/record.rb
+++ lib/rfm/record.rb
@@ -195,7 +195,7 @@ module Rfm
     # When you do, the change is noted, but *the data is not updated in FileMaker*. You must call
     # Record::save or Record::save_if_not_modified to actually save the data.
     def []=(name, value)
-    	name = name.to_s
+    	name = name.to_s.downcase
       return super unless @loaded
       raise Rfm::ParameterError, 
         "You attempted to modify a field that does not exist in the current Filemaker layout." unless self.key?(name)
@@ -216,9 +216,9 @@ module Rfm
     private
 
       def read_attribute(key)
-      	key_string = key.to_s
+      	key_string = key.to_s.downcase
         raise NoMethodError, 
-          "#{key_string} does not exists as a field in the current Filemaker layout." unless (!@layout or !self.keys.grep(/#{key_string}/i).empty?)
+          "#{key_string} does not exists as a field in the current Filemaker layout." unless (!@layout or self.key?(key_string))   #!self.keys.grep(/#{key_string}/i).empty?
         self._old_hash_reader(key).to_s.empty? ? nil : self._old_hash_reader(key) if self._old_hash_reader(key)
       end
 
